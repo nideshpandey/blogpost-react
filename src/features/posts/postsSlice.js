bdb0d3337bc1698a2bd201a7d6438f020ea46e73
nideshpandey <nideshpy@gmail.com>
2022-11-29 23:18:57 +0545

    Added action to reducer

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -10,10 +10,15 @@ const postsSlice = createSlice({
     name: 'posts',
     initialState,
     reducers:{
+        addPost(state,action){
+            state.push(action.payload)
+        }
 
     }
 })
 
 export const selectAllPosts = (state) => state.posts;
+
+export const { addPost } = postsSlice.actions;
 
 export default postsSlice.reducer;
