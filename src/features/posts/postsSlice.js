c03c36dfcc259723fc87536e54a16f38a3930380
nideshpandey <nideshpy@gmail.com>
2022-11-29 23:29:57 +0545

    Logic to add post

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,21 +1,31 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, nanoid } from "@reduxjs/toolkit";
 
 const initialState = [
-    {'id': 1, 'title':'New', 'description':'New description'},
-    {'id': 2, 'title':'New2', 'description':'New description2'},
-]
+  { id: 1, title: "New", description: "New description" },
+  { id: 2, title: "New2", description: "New description2" },
+];
 
 // Creating a slice which holds the logic in the reducers
 const postsSlice = createSlice({
-    name: 'posts',
-    initialState,
-    reducers:{
-        addPost(state,action){
-            state.push(action.payload)
-        }
-
-    }
-})
+  name: "posts",
+  initialState,
+  reducers: {
+    addPost: {
+      reducer(state, action) {
+        state.push(action.payload);
+      },
+      prepare(title, description) {
+        return {
+          payload: {
+            id: nanoid(),
+            title,
+            description,
+          },
+        };
+      },
+    },
+  },
+});
 
 export const selectAllPosts = (state) => state.posts;
 
